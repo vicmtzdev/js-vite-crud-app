35df5ebd52ad96198b92da7476bff96fb81ab501
Victor Martinez <vicmtzdev@gmail.com>
2023-06-17 13:30:18 -0600

    table-html

--- src/users/store/users-store.js
+++ src/users/store/users-store.js
@@ -36,6 +36,15 @@ export default {
     onUserChanged,
     reloadPage,
 
+    /**
+     * 
+     * @returns {User[]}
+     */
     getUsers: () => [...state.users],
+
+    /**
+     * 
+     * @returns {Number}
+     */
     getCurrentPage: () => state.currentPage,
 }
