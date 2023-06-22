7d73285ffc889f29fd2c90f7c3ad2b2dbb80c8cc
Victor Martinez <vicmtzdev@gmail.com>
2023-06-22 14:34:54 -0600

    behavior-modal

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -1,7 +1,22 @@
 import './render-modal.css';
 import modalHtml from './render-modal.html?raw';
 
-let modal;
+let modal, form;
+
+//TODO: Cargar usuario por id
+export const showModal = () => {
+
+    modal?.classList.remove('hide-modal');
+
+}
+
+export const hideModal = () => {
+
+    //TODO: Resete del formulario
+    modal?.classList.add('hide-modal');
+
+}
+
 
 /**
  * 
@@ -14,6 +29,22 @@ export const renderModal = (element) => {
     modal = document.createElement('div');
     modal.innerHTML = modalHtml;
     modal.className = 'modal-container hide-modal';
+
+    form = modal.querySelector('form');
+
+    modal.addEventListener('click', (event) => {
+
+        if (event.target.className !== 'modal-container') return;
+        hideModal();
+
+    });
+
+    form.addEventListener('submit', (event) => {
+
+        event.preventDefault();
+        console.log('Formulario enviado');
+
+    });
 
     element.append(modal);
 
