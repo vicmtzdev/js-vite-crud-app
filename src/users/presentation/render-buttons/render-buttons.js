23df4fefb5ab69ba4fd17dc2c8ba223dfe317881
Victor Martinez <vicmtzdev@gmail.com>
2023-06-22 00:20:38 -0600

    buttons-done

--- src/users/presentation/render-buttons/render-buttons.js
+++ src/users/presentation/render-buttons/render-buttons.js
@@ -1,4 +1,5 @@
 import usersStore from "../../store/users-store";
+import { renderTable } from '../render-table/render-table';
 import './render-buttons.css';
 
 
@@ -19,6 +20,22 @@ export const renderButtons = (element) => {
     currentPageLabel.innerText = usersStore.getCurrentPage();
 
     element.append(prevButton, currentPageLabel, nextButton);
+
+    nextButton.addEventListener('click', async () => {
+
+        await usersStore.loadNextPage();
+        currentPageLabel.innerText = usersStore.getCurrentPage();
+        renderTable(element);
+
+    });
+
+    prevButton.addEventListener('click', async () => {
+
+        await usersStore.loadPreviousPage();
+        currentPageLabel.innerText = usersStore.getCurrentPage();
+        renderTable(element);
+
+    });
 
 
 }
